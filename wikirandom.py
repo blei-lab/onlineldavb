91fc0b7b4f006d02f4a8aa08633b0a776904d1b9
Max Callaghan <max.w.callaghan@gmail.com>
2016-10-11 13:37:12 +0200

    update regex for title

--- wikirandom.py
+++ wikirandom.py
@@ -36,7 +36,7 @@ def get_random_wikipedia_article():
             f = urllib2.urlopen(req)
             while not articletitle:
                 line = f.readline()
-                result = re.search(r'title="Edit this page" href="/w/index.php\?title=(.*)\&amp;action=edit" /\>', line)
+                result = re.search(r'title="Edit this page" href="/w/index.php\?title=(.*)\&amp;action=edit"/\>', line)
                 if (result):
                     articletitle = result.group(1)
                     break
